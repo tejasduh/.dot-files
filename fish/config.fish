336c9e0d04fdae9e6c4ec659ee2633d49b8946b1
Tejas <71956364+tejasduh@users.noreply.github.com>
2026-09-09 16:27:07 +0530

    v1.1

--- fish/config.fish
+++ fish/config.fish
@@ -15,4 +15,9 @@ if status is-interactive
     abbr -a gc git commit -m
     abbr -a gl git log --graph
 
+    alias nvidia-run 'env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only'
 end
+
+
+# Added by Antigravity CLI installer
+set -gx PATH "/home/bakayaro/.local/bin" $PATH
---
+++ sddm/readme.txt
@@ -0,0 +1,1 @@
+https://github.com/stepanzubkov/where-is-my-sddm-theme/tree/main
